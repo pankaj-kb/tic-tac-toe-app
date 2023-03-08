99b5494709bc5d21ed5396f9eaf8bc6ebee21004
pankaj-kb <pankajb2601@gmail.com>
2023-03-09 00:19:41 +0530

    adding basic components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import "./App.css";
+import Icon from "./components/Icon";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import { useState } from "react";
 
-function App() {
-  return (
-    <>
-    </>
-  );
-}
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {
+
+  }
+
+  const checkIsWinner = () => {
+
+  }
+
+  const changeItem = itemNumber => {
+    
+  }
+
+
+  return <Icon />;
+};
 
 export default App;
