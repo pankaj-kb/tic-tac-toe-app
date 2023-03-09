b84977f5e796f7052218566578745389a5fec806
pankaj-kb <pankajb2601@gmail.com>
2023-03-09 23:34:59 +0530

    adding new files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
 import Icon from "./components/Icon";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
 import { useState } from "react";
+import "./App.css";
 
 const itemArray = new Array(9).fill("empty");
 
@@ -12,20 +12,30 @@ const App = () => {
   const [isCross, setIsCross] = useState(false);
   const [winMessage, setWinMessage] = useState("");
 
-  const reloadGame = () => {
+  const reloadGame = () => {};
 
-  }
+  const checkIsWinner = () => {};
 
-  const checkIsWinner = () => {
+  const changeItem = (itemNumber) => {};
 
-  }
-
-  const changeItem = itemNumber => {
-    
-  }
-
-
-  return <Icon />;
+  return (
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+        <Row>
+          <Col md={6} className="offset-md-3">
+            <div className="grid">
+              {itemArray.map((item, index) => (
+                <Card>
+                  <CardBody className="box">
+                  <Icon name={item} />
+                  </CardBody>
+                </Card>
+              ))}
+            </div>
+          </Col>
+        </Row>
+    </Container>
+  );
 };
 
 export default App;
